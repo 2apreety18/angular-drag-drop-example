c73f97183e321de54f41e18507dd09ff011195f6
2apreety18 <2apreety@gmail.com>
2023-01-26 23:39:58 +0600

    Added emoji

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,19 +8,44 @@ import { Component} from '@angular/core';
 })
 export class AppComponent{
 
-  letters = [
-    'C',
-    'O',
-    'D',
-    'E'
-  ];
+  // letters = [
+  //   'C',
+  //   'O',
+  //   'D',
+  //   'E'
+  // ];
+
+  // containerId: string | undefined;
+
+  // drop(event: CdkDragDrop<string[]>) {
+  //   moveItemInArray(this.letters, event.previousIndex, event.currentIndex);
+  //   this.containerId = event.container.id;
+  //   console.log(event);
+  //   console.log(event.container.id);
+  //   console.log(event.previousIndex);
+  //   console.log(event.currentIndex);
+  // }
+
+  fruits = ['🍊', '🍉', '🍋', '🍇','🍏'];
+  fastFoods = ['🍔', '🍟', '🍕', '🌮', '🌭'];
+
+  containerId: string | undefined;
+
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.letters, event.previousIndex, event.currentIndex);
-    console.log(event);
-    console.log(event.container.id);
-    console.log(event.previousIndex);
-    console.log(event.currentIndex);
+    //if the drag & Drop event has happenedin the same container (previous container === container)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      this.containerId = event.container.id;
+
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
   
 }
